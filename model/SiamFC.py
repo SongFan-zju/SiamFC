6326b7486d62a26d7f64e30125cd0fa3604d4318
songfan <songfan.nothing@gmail.com>
2025-03-15 13:21:16 +0800

    model

--- model/SiamFC.py
+++ model/SiamFC.py
@@ -10,3 +10,16 @@ class BackBone(nn.Module):
         super(BackBone, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, 3, 1, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(16, 16, 3, 1, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(16, 16, 3, 1, 1, bias=False)
+        self.bn3 = nn.BatchNorm2d(16)
+        self.relu3 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.relu2(self.bn2(self.conv2(x)))
+        x = self.relu3(self.bn3(self.conv3(x)))
+        return x
